1bee4229f594b53d355a5f7404cf04d7e95f8af3
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-06-27 02:18:14 +0700

    menambah code untuk mengubah maksimal perhitungan

--- app/src/main/java/com/example/aplikasitasbih/MainActivity.java
+++ app/src/main/java/com/example/aplikasitasbih/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.aplikasitasbih;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int count =0;
-    private Button btnKurang, btnTambah, btnReset;
-    private TextView hasilHitung;
+    int oldValue;
+    int count = 0;
+    private Button btnKurang, btnTambah, btnReset, btnUbah;
+    private TextView hasilHitung,textUbah, txtper;
+    int i = 0;
+
 
 
     @Override
@@ -17,10 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnKurang =findViewById(R.id.btnKurang);
-        btnTambah =findViewById(R.id.btnTambah);
-        hasilHitung= findViewById(R.id.hasil);
-        btnReset=findViewById(R.id.btnReset);
+        btnKurang = findViewById(R.id.btnKurang);
+        btnTambah = findViewById(R.id.btnTambah);
+        hasilHitung = findViewById(R.id.hasil);
+        btnReset = findViewById(R.id.btnReset);
+        textUbah = (TextView) findViewById(R.id.perberapa);
+        btnUbah = findViewById(R.id.btnUbah);
+        txtper=findViewById(R.id.per);
+
 
         btnKurang.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +44,67 @@ public class MainActivity extends AppCompatActivity {
                 hasilHitung.setText(Integer.toString(count));
             }
         });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                hasilHitung.setText(Integer.toString(count));
+
+            }
+        });
+
+        btnUbah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (i == 0) {
+                    textUbah.setText("33") ;
+                    btnUbah.setText("33");
+                    txtper.setText("/");
+                    hasilHitung.setText("0");
+                    i++;
+                } else if (i == 1) {
+                    textUbah.setText("99");
+                    btnUbah.setText("99");
+                    txtper.setText("/");
+                    hasilHitung.setText("0");
+                    i = 2;
+                } else if (i ==2 ){
+                    textUbah.setText("");
+                    btnUbah.setText("~");
+                    txtper.setText("");
+                    hasilHitung.setText("0");
+                    i = 0;
+
+                }
+            }
+        });
+
+        //ubahHasilButton();
+
+
     }
+
+//    private void ubahHasilButton() {
+//        final String[] beberapaUbahan ={"33", "99", ""};
+//        final TextView textUbah = (TextView) findViewById(R.id.perberapa);
+//        btnUbah = findViewById(R.id.btnUbah);
+//
+//        btnUbah.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int random = (int) (Math.random() * beberapaUbahan.length);
+//                if (random == oldValue){
+//                    random=(int) (Math.random() * beberapaUbahan.length);
+//                }
+//                textUbah.setText(beberapaUbahan[random]);
+//                oldValue = random;
+//
+//
+//            }
+//
+//        });
+//    }
+
+
 }
